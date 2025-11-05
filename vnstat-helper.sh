12bf38dd5b8d6f7ff0c70988ef0eb16668dfc103
Abdul Mannan Masood <ammasood12@gmail.com>
2025-11-06 03:24:22 +0800

    Update vnstat-helper.sh

--- vnstat-helper.sh
+++ vnstat-helper.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # ───────────────────────────────────────────────
 # CONFIGURATION
 # ───────────────────────────────────────────────
-VERSION="2.8.6.1"
+VERSION="2.8.7"
 BASE_DIR="/root/vnstat-helper"
 SELF_PATH="$BASE_DIR/vnstat-helper.sh"
 DATA_FILE="$BASE_DIR/baseline"
